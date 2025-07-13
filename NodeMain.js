7b199c944eceddfe9d594ce6362528789102b3ea
Chunnu Ram <chunnuram403@gmail.com>
2025-07-13 14:01:45 +0530

    my node file

---
+++ NodeMain.js
@@ -0,0 +1,4 @@
+console.log(    "Hello, World! This is a simple Node.js application.");
+console.log("This application is designed to demonstrate a basic CI/CD pipeline process.");
+console.log("You can modify this code to add more functionality as needed.");
+console.log("Feel free to expand this application with additional features or logic.");
