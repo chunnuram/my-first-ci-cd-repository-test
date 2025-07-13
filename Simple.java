2673f234b14e2cfc3573b0126a814601d817e268
Chunnu Ram <chunnuram403@gmail.com>
2025-07-13 13:33:33 +0530

    second class simple.java added

---
+++ Simple.java
@@ -0,0 +1,9 @@
+
+public class Simple {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World! simple Java program.");
+        // This is a simple Java program that prints "Hello, World!" to the console.
+     
+    }
+}
