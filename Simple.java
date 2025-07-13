93ce6dbdfced3fe8935a16d2ee4dadf1e36c655d
Chunnu Ram <chunnuram403@gmail.com>
2025-07-13 14:14:49 +0530

    tests

--- Simple.java
+++ Simple.java
@@ -2,7 +2,7 @@
 public class Simple {
     
     public static void main(String[] args) {
-        System.out.println("Hello, World! simple Java program.")
+        System.out.println("Hello, World! simple Java program.");
         // This is a simple Java program that prints "Hello, World!" to the console.
      
     }
