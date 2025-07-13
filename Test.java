c65a8bf40be410888e5b2704dc13bf551b4f391c
Chunnu Ram <chunnuram403@gmail.com>
2025-07-13 12:05:11 +0530

    test for ci-cd pipeline

---
+++ Test.java
@@ -0,0 +1,11 @@
+
+class Test{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // This is a simple Java program that prints "Hello, World!" to the console.
+        System.out.println("This is a test of the Java program structure.");
+        // You can add more functionality here as needed.
+        System.out.println("Feel free to modify and expand this code.");
+        // Remember to use the correct case for 'System' to avoid compilation errors.
+    }
+}
